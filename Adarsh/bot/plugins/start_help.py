36616ca050138889efb0eece817cedd7aae0688a
Raman456123 <103272817+Raman456123@users.noreply.github.com>
2022-05-26 09:10:57 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -119,7 +119,7 @@ async def help_handler(bot, message):
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [Vivek Tomar](https://t.me/ChVivekTomar).",
+                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [Pravinboopathi](https://t.me/Pravin_boopathi).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
